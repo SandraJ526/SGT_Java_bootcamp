55d46359ce949f3d453b0601cfee64594acde5dc
SandraJ526 <sandra.jegina@gmail.com>
2020-03-17 22:42:37 +0200

    Add files via upload

---
+++ Task_2/src/learn/programming/Main.java
@@ -0,0 +1,19 @@
+package learn.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean remoteLearning = true;
+        int studentGroup = 4;
+
+        if (remoteLearning) {
+
+            double startDate = 3.16;
+            String programmName = "She Goes Tech Java bootcamp";
+
+            System.out.println("It's " + remoteLearning + "- " + programmName + " for group " + studentGroup +
+                    " started this year " + startDate + ".");
+        }
+    }
+}
