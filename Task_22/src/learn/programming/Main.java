9b5aa623f748344ac7a262c20e021e87db275b1d
SandraJ526 <sandra.jegina@gmail.com>
2020-03-22 22:50:05 +0200

    Add files via upload

---
+++ Task_22/src/learn/programming/Main.java
@@ -0,0 +1,15 @@
+package learn.programming;
+
+
+enum Days {
+    PIEKTDIENA
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Days day = Days.PIEKTDIENA;
+        System.out.println(day);
+    }
+}
