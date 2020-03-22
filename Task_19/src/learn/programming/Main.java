8c74cdebeb6822f8fa07471bbbaeac494d03b69f
SandraJ526 <sandra.jegina@gmail.com>
2020-03-22 21:57:15 +0200

    Add files via upload

---
+++ Task_19/src/learn/programming/Main.java
@@ -0,0 +1,12 @@
+package learn.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array = {100, 200, 233, 66, 77, 98};
+
+        System.out.println(array[0]);
+        System.out.println(array[array.length-1]);
+    }
+}
