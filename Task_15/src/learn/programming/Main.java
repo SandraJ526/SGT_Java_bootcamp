ef6ca703853c76516d422d995a0d7b808c1ad2b4
SandraJ526 <sandra.jegina@gmail.com>
2020-03-22 19:24:57 +0200

    Add files via upload

---
+++ Task_15/src/learn/programming/Main.java
@@ -0,0 +1,13 @@
+package learn.programming;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array = {1,6,7,78,9,9};
+
+        System.out.println(Arrays.toString(array));
+    }
+}
