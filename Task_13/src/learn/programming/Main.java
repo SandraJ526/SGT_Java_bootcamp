7648cfb30ce23e2dac7cd3a4e244e03af9b523d4
SandraJ526 <sandra.jegina@gmail.com>
2020-03-23 12:07:50 +0200

    Add files via upload

--- Task_13/src/learn/programming/Main.java
+++ Task_13/src/learn/programming/Main.java
@@ -14,9 +14,9 @@ public class Main {
 
         System.out.println("Type of spruce: Norway (1) or Blue (2)? ");
         treeType = sc.nextInt();
+
         if (treeType == 2) {
             System.out.println("Place it in the fireplace hall");
-            return;
 
         } else if (treeType == 1) {
             System.out.println("Please enter height in cm: ");
@@ -24,15 +24,18 @@ public class Main {
             System.out.println("Please enter diameter ir cm: ");
             diameter = sc.nextInt();
 
-
-            if (height >= 50 || height < 100 && diameter >= 100 || diameter < 150) {
-                System.out.println("Place it in the living room 1");
-            } else if (height >= 100 || height < 150 && diameter >= 150 || diameter < 200) {
+            if (height >= 150 && height <= 300 && diameter >= 200 && diameter <= 300) {
+                System.out.println("Place it in the fireplace hall");
+                return;
+            } else if (height >= 100 && height <= 150 && diameter >= 150 && diameter < 200) {
                 System.out.println("Place it in the living room 2");
-            } else if (height >= 150 || height <= 300 && diameter >= 200 || diameter <= 300) {
-                System.out.println("Place it in the living room 3");
+                return;
+            } else if (height >= 50 && height < 100 && diameter >= 100 && diameter < 150) {
+                System.out.println("Place it in the living room 1");
+                return;
             } else {
                 System.out.println("Find a special place for this tree");
+                return;
             }
         }
     }
