7df0c8707ea227559932079abc75eab6b2bb7852
SandraJ526 <sandra.jegina@gmail.com>
2020-04-08 18:04:32 +0300

    Add files via upload

---
+++ Task_54/src/learn/programming/Main.java
@@ -0,0 +1,13 @@
+package learn.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var result = SumCalculator.calculateSum(5, 9);
+        System.out.println(result);
+
+        result = SumCalculator.calculateSum(98, 4);
+        System.out.println(result);
+    }
+}
---
+++ Task_54/src/learn/programming/SumCalculator.java
@@ -0,0 +1,8 @@
+package learn.programming;
+
+public class SumCalculator {
+
+    public static int calculateSum(int x, int y) {
+        return x + y;
+    }
+}
