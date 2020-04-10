fae37f82a7b09284ee46281bf19e6ba174a84921
SandraJ526 <sandra.jegina@gmail.com>
2020-04-10 20:58:16 +0300

    Add files via upload

---
+++ Task_57/src/learn/programming/Main.java
@@ -0,0 +1,17 @@
+package learn.programming;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100) + 1;
+            i++;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
