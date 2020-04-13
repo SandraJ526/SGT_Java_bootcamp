4d08bbf8424f51a3506a90b5f927d669176ea87b
SandraJ526 <sandra.jegina@gmail.com>
2020-04-13 23:07:20 +0300

    Add files via upload

--- Task_57/src/learn/programming/Main.java
+++ Task_57/src/learn/programming/Main.java
@@ -9,8 +9,7 @@ public class Main {
         int[] array = new int[10];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100) + 1;
-            i++;
+            array[i] = (int) (Math.random() * 100)- 10;
         }
         System.out.println(Arrays.toString(array));
     }
